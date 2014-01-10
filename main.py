5468c5c77af01d9773c9726c616b687dd10fd379
brenden17 <chulwook.jeon@gmail.com>
2014-01-10 20:14:12 +0900

    update

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-from models import Post
-
 # sys.path includes 'server/lib' due to appengine_config.py
 from flask import Flask
 from flask import render_template
 
+from models import Post
+k
 app = Flask(__name__.split('.')[0])
 
 
