505d45e6a60e7ca1a25d6d8c14534afb60bfa9bf
brenden17 <chulwook.jeon@gmail.com>
2014-01-10 22:33:43 +0900

    a

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__.split('.')[0])
 @app.route('/index')
 def index():
     """ Return hello template at application root URL."""
-    return render_template('index.html')
+    return render_template('hello.html')
 
 @app.route('/blog')
 def post():
